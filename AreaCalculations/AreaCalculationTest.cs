4127a4668d1e5140ba32d9e4d6d6cd9574f6f919
cs4171 <cs4171@SKCS4171>
2017-10-12 14:28:16 +0800

    Add a failed test case : calculate triangle area.

---
+++ AreaCalculations/AreaCalculationTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculations
+{
+    [TestFixture]
+    public class AreaCalculationTest
+    {
+        private Calculator sut = new Calculator();
+
+        [Test]
+        public void Area_Of_A_Triangle_Is_Half_Of_Its_Base_Multiplied_By_Height()
+        {
+            const double triangleBase = 6;
+            const double triangleHeight = 4;
+
+            var triangle = new Triangle(triangleBase, triangleHeight);
+
+            Assert.AreEqual(12, sut.GetTotalArea(triangle));
+        }
+    }
+
+}
---
+++ AreaCalculations/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AreaCalculations
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        internal double GetTotalArea(Triangle triangle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Triangle
+    {
+        private double triangleBase;
+        private double triangleHeight;
+
+        public Triangle(double triangleBase, double triangleHeight)
+        {
+            this.triangleBase = triangleBase;
+            this.triangleHeight = triangleHeight;
+        }
+    }
+}
